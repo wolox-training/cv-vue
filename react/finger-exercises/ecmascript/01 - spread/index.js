31998c07937d682d71752028b71211f8494cacd8
Cristian Vega <cristianvega@Woloxs-MacBook-Pro-48.local>
2018-11-29 09:07:20 -0500

    second test

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -1,8 +1,11 @@
 import { isArray } from './utils';
 
 export function min(args) {
-  // isArray();
-  return Math.min(args);
+  console.log(args, 'argumentos')
+  if(isArray(args))
+    return Math.min(...args);
+  else 
+    return Math.min(args)
 }
 
 export function copy() {
