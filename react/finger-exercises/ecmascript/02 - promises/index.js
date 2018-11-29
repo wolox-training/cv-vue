99e3533e792a68407df3956f17dd4d283d13f8fa
Cristian Vega <cristianvega@Woloxs-MacBook-Pro-48.local>
2018-11-29 10:36:16 -0500

    first test promises

--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,7 +1,11 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  const refTime = Date.now();
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(Date.now() - refTime)
+    , time)
+  })
 }
 
 export function asyncDelay() {
