f2ffc0fb67c088c1ec6cd1c7ced7c81b2c09528d
Cristian Vega <cristianvega@Woloxs-MacBook-Pro-48.local>
2018-12-18 18:49:59 -0500

    solved comments of trainner

--- react/tic-tac-toe/src/app/components/CustomRoute/index.js
+++ react/tic-tac-toe/src/app/components/CustomRoute/index.js
@@ -4,7 +4,7 @@ import { Route, Redirect } from 'react-router-dom';
 
 function CustomRoute ({isPrivate, auth, path, component: Component, exact}) {
     if (isPrivate && auth)
-      return <Route exact={!!exact} path={path} component={Component} />
+      return <Route exact={exact} path={path} component={Component} />
     else if (isPrivate && !auth)
       return <Redirect to="/login" />
     else if(!isPrivate && !auth)
@@ -17,7 +17,7 @@ CustomRoute.propTypes = {
   isPrivate: PropTypes.bool,
   auth: PropTypes.bool,
   path: PropTypes.string,
-  component: PropTypes.object,
+  component: PropTypes.func,
   exact: PropTypes.bool
 }
 
