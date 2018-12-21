6f8679f12a2d26839cf07d5eb73c75e817385971
Cristian Vega <cristianvega@Woloxs-MacBook-Pro-48.local>
2018-12-21 15:31:23 -0500

    move HOC loading from login component to formLogin component making it more modular

--- react/tic-tac-toe/src/app/screens/App/screens/Login/components/FormLogin/index.js
+++ react/tic-tac-toe/src/app/screens/App/screens/Login/components/FormLogin/index.js
@@ -3,6 +3,7 @@ import { Field, reduxForm } from 'redux-form';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import FormField from '../Field';
+import WithLoading from 'HOC/loading';
 import { required, isMail, minLength } from 'validations';
 import cx from 'classnames';
 
@@ -26,29 +27,29 @@ class LoginForm extends Component {
     }
 
     return (
-      <div className={styles.containerForm}>
-        <form onSubmit={this.props.handleSubmit}>
-          <Field 
-            name="email" 
-            component={FormField} 
-            type="text" 
-            className={styles.fieldInput} 
-            label="Email"
-            validate={[required,isMail]} 
-          />
-          <Field 
-            name="password" 
-            component={FormField} 
-            type="password" 
-            className={styles.fieldInput} 
-            label="Password"
-            validate={[required, minLength]}
-          />
-          <button type="submit" className={getClassName(this.props)} >Log In</button>
-        </form>
-        <span className={styles.formStatus} >{getMessage()}</span>
-      </ div>
-    )
+        <div className={styles.containerForm}>
+          <form onSubmit={this.props.handleSubmit}>
+            <Field 
+              name="email" 
+              component={FormField} 
+              type="text" 
+              className={styles.fieldInput} 
+              label="Email"
+              validate={[required,isMail]} 
+            />
+            <Field 
+              name="password" 
+              component={FormField} 
+              type="password" 
+              className={styles.fieldInput} 
+              label="Password"
+              validate={[required, minLength]}
+            />
+            <button type="submit" className={getClassName(this.props)} >Log In</button>
+          </form>
+          <span className={styles.formStatus} >{getMessage()}</span>
+        </ div>
+      )
   }
 }
 
@@ -61,8 +62,9 @@ LoginForm.propTypes = {
   status: PropTypes.string
 };
 
-const mapStateToProps = ({user :{ status }}) => ({
-  status
+const mapStateToProps = ({user :{ status, tokenLoading }}) => ({
+  status,
+  tokenLoading
 });
 
-export default connect(mapStateToProps)(LoginForm);
+export default WithLoading(connect(mapStateToProps)(LoginForm));
--- react/tic-tac-toe/src/app/screens/App/screens/Login/index.js
+++ react/tic-tac-toe/src/app/screens/App/screens/Login/index.js
@@ -3,10 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import LoginForm from './components/FormLogin';
 import actions from 'redux/login/actions';
-import WithLoading from 'HOC/loading'
 import { tokenExist } from 'app/screens/App/utils';
-
-const LoginFormWithLoading = WithLoading(LoginForm);
 
 class Login extends Component {
   handleSubmit = (values) => {
@@ -22,7 +19,7 @@ class Login extends Component {
   render() {
     return (
       <>
-        <LoginFormWithLoading isLogin={this.props.tokenLoading} onSubmit={this.handleSubmit} />
+        <LoginForm isLogin={this.props.tokenLoading} onSubmit={this.handleSubmit} />
       </>
     )
   }
